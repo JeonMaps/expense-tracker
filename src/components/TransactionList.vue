2306d7286b5044cc420dddcfc8b1eac037bd1f0e
Jeon Maps <65327532+JeonMaps@users.noreply.github.com>
2024-11-11 18:16:00 +0800

    Components and functionality

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,27 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li v-for="transaction in transactions" v-bind:key="transaction.id"
+            :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>P{{ transaction.amount }}</span>
+            <button class="delete-btn" @click="deleteTransaction(transaction.id)">x</button>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const emit = defineEmits(['deleteTransaction'])
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    }
+})
+
+const deleteTransaction = (id) => {
+    emit('deleteTransaction', id)
+}
+</script>
